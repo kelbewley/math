ae768a23f2535c51dbed16c9afcf1281f036d71b
Kelly Bewley <splishswimmer128@Kellys-MacBook-Pro-3.local>
2016-02-24 09:44:18 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Math program for demponstrating TDD
+
+This is a short program that does basic arithmatic and demonstrates use of test-driven development (TDD)
---
+++ spec/math_spec.js
@@ -0,0 +1,8 @@
+// Tell the test where to find the code
+var math = require('../math');
+
+describe("A simple program that does arithmatic", function ()
+
+	exports.add = function(a,b){
+		return a + b;
+	};
