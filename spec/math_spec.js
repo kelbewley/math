d372d8ba5685bfca312afd070186072d6c3b43b2
kelbewley <kelbewley@gmail.com>
2016-05-03 19:56:32 -0400

    Added files via upload

--- spec/math_spec.js
+++ spec/math_spec.js
@@ -3,6 +3,19 @@ var math = require('../math');
 
 describe("A simple program that does arithmatic", function ()
 
-	exports.add = function(a,b){
-		return a + b;
-	};
+	
+  it("can add two numbers", function() {
+    expect(math.add(4,5)).toBe(9);
+  });
+
+  it("can subtract two numbers", function() {
+  	expect(math.subtract(10,5)).toBe(5);
+  });
+
+  it("can multiply two numbers", function() {
+    expect(math.multiply(3,5)).toBe(15);
+  });
+
+  it("can divide two numbers", function() {
+  	expect(math.divide(10,2)).toBe(5);
+  });
